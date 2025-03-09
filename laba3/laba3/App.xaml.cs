df209a13243e4d0309ef8a246566767861b2bbf5
Moz4ch <perekrestovdanik@gmail.com>
2025-03-09 22:31:50 +0700

    Add files via upload

---
+++ laba3/laba3/App.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Windows;
+
+namespace UniversityManagementSystem
+{
+    public partial class App
+    {
+        [STAThread]
+        public static void Main()
+        {
+            var app = new App();
+            app.InitializeComponent();
+            app.Run();
+        }
+    }
+}
---
+++ laba3/laba3/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System.Windows;
+
+namespace UniversityManagementSystem
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            LoadStudents();
+        }
+
+        private void LoadStudents()
+        {
+            StudentsListView.ItemsSource = null; 
+            StudentsListView.ItemsSource = StudentRepository.Students;
+        }
+
+        private void AddStudentButton_Click(object sender, RoutedEventArgs e)
+        {
+            var addStudentWindow = new AddStudentWindow();
+            addStudentWindow.ShowDialog();
+            LoadStudents();
+        }
+
+        private void EditStudentButton_Click(object sender, RoutedEventArgs e)
+        {
+            var selectedStudent = StudentsListView.SelectedItem as Student;
+            if (selectedStudent != null)
+            {
+                var editStudentWindow = new EditStudentWindow(selectedStudent);
+                editStudentWindow.ShowDialog();
+                LoadStudents();
+            }
+            else
+            {
+                MessageBox.Show("Выберите студента для редактирования");
+            }
+        }
+        private void ViewApplicationsButton_Click(object sender, RoutedEventArgs e)
+        {
+            var applicationsWindow = new ApplicationsWindow();
+            applicationsWindow.ShowDialog();
+        }
+
+        private void AssignResponsibleButton_Click(object sender, RoutedEventArgs e)
+        {
+            var assignResponsibleWindow = new AssignResponsibleWindow();
+            assignResponsibleWindow.ShowDialog();
+        }
+    }
+}
